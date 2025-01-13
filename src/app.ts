af847fefbe3c776fa988152b9cb0aed584f4f453
nurhossainfarid <faahsan1516@gmail.com>
2025-01-13 15:40:16 +0600

    add eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,22 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors';
-const app: Application = express()
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
 
 // parser
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello Blogging!')
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Blogging!");
+});
 
 // Wrong route
 app.all("*", (req: Request, res: Response) => {
   res.status(400).json({
     success: false,
-    message: "Route not found"
-  })
-})
+    message: "Route not found",
+  });
+});
 
 // Global error handler
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -25,9 +24,10 @@ app.use((error: any, req: Request, res: Response) => {
   if (error) {
     res.status(400).json({
       success: false,
-      message: "Something went wrong"
-    })
+      message: "Something went wrong",
+    });
   }
-})
+});
 
-export default app
+
+export default app;
